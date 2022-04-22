89473d2701acc29cfd145c53c3ae461129a72e00
Gabriel Peruchi <gabriel-peruchi@hotmail.com>
2022-04-21 22:06:19 -0300

    PageTitle

---
+++ frontend/src/components/template/PageTitle.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="page-title">
+    <h1><v-icon v-if="icon" :icon="icon" />{{ title }}</h1>
+    <h2>{{ subtitle }}</h2>
+    <hr/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from '@vue/runtime-core'
+
+defineProps<{
+  icon: string,
+  title: string,
+  subtitle: string
+}>()
+
+</script>
+
+<style>
+.page-title h1 {
+  margin: 0;
+}
+
+.page-title h2 {
+  color: #777;
+  margin-top: 5px;
+  font-size: 1.3rem;
+}
+</style>
